a695b2eeda0dc4f1db427969e88d6d9a1442bc04
kanaee-cloud <arsalnaufal2007@gmail.com>
2024-02-14 21:10:00 +0700

    cape

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     fontFamily: {
       primary: 'Poppins',
       secondary: 'Montserrat',
-      tertiary: 'Revamped',
+      tertiary: 'Poppins',
     },
     container: {
       padding: {
